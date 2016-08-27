428f46376e8104558e9acb0506bb3d5e8939c531
George Lesica <george@lesica.com>
2016-08-27 12:53:00 -0600

    Implement working runner.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM ubuntu:16.04
 MAINTAINER George Lesica
 RUN apt-get -y update && apt-get install -y \
     openjdk-8-jdk-headless
-ENTRYPOINT javarun
+ENTRYPOINT ["javarun"]
 COPY javarun.sh /usr/bin/javarun
 
--- javarun.sh
+++ javarun.sh
@@ -5,8 +5,24 @@ set -e
 srcpath="$1"
 exepath=${srcpath//.java/}
 
+echo "Input:"
+echo ""
+echo "$srcpath"
+echo ""
+
 cat - > "$srcpath"
 
+echo "Program:"
+echo ""
+cat "$srcpath"
+echo ""
+
+echo "Compile:"
+echo ""
 javac "$srcpath"
+echo ""
+
+echo "Output:"
+echo ""
 java "$exepath"
 
